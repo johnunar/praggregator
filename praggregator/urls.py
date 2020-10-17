48cdaa565187cb60455503db71046bf28e6ac00c
Jan Unar <johnunar@gmail.com>
2020-10-17 16:28:10 +0200

    Added generated documentation.

--- praggregator/urls.py
+++ praggregator/urls.py
@@ -1,10 +1,24 @@
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
+from drf_yasg2 import openapi
+from drf_yasg2.views import get_schema_view
+from rest_framework import routers, permissions
 from rest_framework.authtoken import views
 
 import praggregator.views
 from app.views import ProductViewSet, UserViewSet, OfferViewSet
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Product Aggregator API",
+        default_version='v1',
+        description="Test description",
+        contact=openapi.Contact(email="johnunar@gmail.com"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 router = routers.DefaultRouter()
 router.register(r'products', ProductViewSet)
@@ -16,4 +30,6 @@ urlpatterns = [
     path('signup/', praggregator.views.signup, name='signup'),
     path('api/v1/', include(router.urls)),
     path('api/v1/auth/', views.obtain_auth_token, name='auth'),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
